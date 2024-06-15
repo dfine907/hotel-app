7ea5cbef429c2021a03d012cf92230e41cb35ead
Dina <dinagiacobbi@gmail.com>
2024-06-15 08:26:46 -0800

    Adding image folder and image

--- app/tours/[id]/page.tsx
+++ app/tours/[id]/page.tsx
@@ -1,9 +1,27 @@
+import mapsImg from '@/images/maps.jpg'
+import Image from 'next/image'
 
-
-function page({params}:{params:{id:string}}) {
+function page({ params }: { params: { id: string } }) {
   return (
     <div>
-        <h1 className="text-4xl">ID : {params.id}</h1>
+      <h1 className="text-4xl">ID : {params.id}</h1>
+      <section className="flex gap-x-4 mt-4">
+        {/* local image */}
+        <div>
+          <Image
+            src={mapsImg}
+            alt="maps"
+            priority
+            width={192}
+            height={192}
+            className="w-48 h-48 object-cover rounded"
+          />
+          <h2>Local Image </h2>
+        </div>
+
+        {/* remote image */}
+        <div></div>
+      </section>
     </div>
   )
 }
