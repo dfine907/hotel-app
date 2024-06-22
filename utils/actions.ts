f26fa90d4da93d3df87347654f1f1287442fd3f6
Dina <dinagiacobbi@gmail.com>
2024-06-22 13:14:34 -0800

    Adding validation with redirect paths

--- utils/actions.ts
+++ utils/actions.ts
@@ -1,37 +1,45 @@
 'use server'
-
 import { readFile, writeFile } from 'fs/promises'
+import { revalidatePath } from 'next/cache'
+import { redirect } from 'next/navigation'
 
 type User = {
   id: string
   firstName: string
   lastName: string
 }
-
 const createUser = async (formData: FormData) => {
   'use server'
   const firstName = formData.get('firstName') as string
   const lastName = formData.get('lastName') as string
-  // const rawData = Object.fromEntries(formData)
-  // console.log(rawData)
+  const newUser: User = {
+    firstName,
+    lastName,
+    id: Date.now().toString(),
+  }
 
-  const newUser: User = {firstName, lastName, id: Date.now().toString()}
+  try {
+    await saveUser(newUser)
+    // some logic
+  } catch (error) {
+    console.log(error)
+  }
 
- await saveUser(newUser)
-  
+  // OPTION 1:  revalidatePath('/actions')
+  //Option 2 (but not inside try, catch:)
+  redirect('/')
 }
-export const fetchUsers = async():Promise<User[]> => {
-  const result = await readFile('users.json', {encoding: 'utf-8'})
-  const users = result? JSON.parse(result) : []
+
+export const fetchUsers = async (): Promise<User[]> => {
+  const result = await readFile('users.json', { encoding: 'utf-8' })
+  const users = result ? JSON.parse(result) : []
   return users
 }
-
 
 const saveUser = async (user: User) => {
   const users = await fetchUsers()
   users.push(user)
   await writeFile('users.json', JSON.stringify(users))
-  
 }
 
 export default createUser
