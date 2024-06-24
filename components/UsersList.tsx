c45c98c1d35783009e27fa20011d7e5f427746f9
Dina <dinagiacobbi@gmail.com>
2024-06-24 06:54:06 -0800

    Adding delete button component

--- components/UsersList.tsx
+++ components/UsersList.tsx
@@ -1,4 +1,5 @@
 import { fetchUsers } from '@/utils/actions'
+import DeleteButton from './DeleteButton'
 
 async function UsersList() {
   const users = await fetchUsers()
@@ -7,8 +8,9 @@ async function UsersList() {
     <div className="mt-4 ">
       {users.length? <div>
         {users.map((user)=> {
-          return <h4 key={user.id} className=' capitalize text-lg' >
+          return <h4 key={user.id} className=' capitalize text-lg flex justify-between items-center mb-2' >
             {user.firstName} {user.lastName}
+            <DeleteButton id={user.id} />
           </h4>
         })}
       </div> : <p> No users found ... </p>}
