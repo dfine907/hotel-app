def6bfb99f80dfa68c4f6863a9a9b10e9e07182f
Dina <dinagiacobbi@gmail.com>
2024-06-25 14:43:29 -0800

    Adding initial routing

---
+++ app/api/users/route.ts
@@ -0,0 +1,7 @@
+import { NextRequest, NextResponse } from 'next/server';
+import { fetchUsers, saveUser } from '@/utils/actions';
+
+export const GET = async ()=> {
+    const users = await fetchUsers()
+    return Response.json({users})
+}
